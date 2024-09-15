52bb09693c6a2d48821312a899c0b09d93803b75
cheakf <sunmoming@gmail.com>
2024-09-15 14:51:57 +0800

    相机读取测试完成

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <fstream>
+
+#include "camera.hpp"
+#include "nlohmann/json.hpp"
 
 int main()
 {
-    std::cout << "hello" << std::endl;
+    toe::hik_camera temp;
+    std::ifstream f("../config.json");
+    nlohmann::json temp_apra = nlohmann::json::parse(f);
+    temp.hik_init(temp_apra,0);
+    int k = 0;
+    cv::Mat img;
+    while (k != 27)
+    {
+        frame_mutex.lock();
+        img = frame_rgb;
+        frame_mutex.unlock();
+        if (img.data)
+        {
+            cv::imshow("frame",img);
+        }
+        k = cv::waitKey(1);
+    }
+    temp.hik_end();
     return 0;
 }
