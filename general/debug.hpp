d8f64f0ae5074bdf625751dcb951d17bce251feb
cheakf <sunmoming@gmail.com>
2024-09-15 22:58:26 +0800

    多线程框架大体调试完成

--- general/debug.hpp
+++ general/debug.hpp
@@ -1,2 +1,13 @@
 // 这里都是调试用的标志位
 
+//---------------------------------默认参数，一般不更改---------------------------------//
+// 默认的识别颜色
+#define DEFALUTE_COLOR 0
+// 默认的识别模式
+#define DEFALUTE_MODE 0
+// chono系统时钟调用单位转换常数
+#define TIME_TRANSFORMER 1000000000.0
+// 该项目用于对付傻逼vscode的c++智能补全抽风，一般不用动
+#ifndef PROJECT_PATH
+    #define PROJECT_PATH "/home/cheakf/toe_aimbot/"
+#endif
--- main.cpp
+++ main.cpp
@@ -1,29 +1,64 @@
 #include <iostream>
+#include <string>
+#include <thread>
 #include <fstream>
 
 #include "camera.hpp"
 #include "nlohmann/json.hpp"
+#include "serial.hpp"
 
-int main()
+toe::hik_camera hik_cam;
+nlohmann::json config;
+
+int mode = 0;
+int color = 0;
+
+void detect_process(void)
 {
-    toe::hik_camera temp;
-    std::ifstream f("../config.json");
-    nlohmann::json temp_apra = nlohmann::json::parse(f);
+    // 编写推理和处理代码
+}
+
+void grab_img(void)
+{
     int device_num = 0;
-    temp.hik_init(temp_apra, device_num);
+    hik_cam.hik_init(config, device_num);
+    // 重启一次防止不正常退出后启动异常
+    hik_cam.hik_end();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    hik_cam.hik_init(config, device_num);
     int k = 0;
-    cv::Mat img;
     while (k != 27)
     {
         mutex_array[device_num].lock();
-        img = frame_array[device_num];
+        cv::Mat frame = frame_array[device_num];
         mutex_array[device_num].unlock();
-        if (img.data)
+        if(frame.size[0] > 0)
         {
-            cv::imshow("frame",img);
+            // 这里记得将图像传递到其他线程
+            cv::imshow("1", frame);
+            k = cv::waitKey(1);
         }
-        k = cv::waitKey(1);
     }
-    temp.hik_end();
+    hik_cam.hik_end();
+    cv::destroyAllWindows();
+}
+
+int main()
+{
+
+    // 为了提升cout的效率关掉缓存区同步，此时就不能使用c风格的输入输出了，例如printf
+    // oi上常用的技巧，还有提升输出效率的就是减少std::endl和std::flush的使用
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(0);
+
+    std::cout << PROJECT_PATH << std::endl;
+    std::ifstream f(std::string(PROJECT_PATH) + std::string("/config.json"));
+    config = nlohmann::json::parse(f);
+
+    std::thread grab_thread = std::thread(grab_img);
+    grab_thread.detach();
+    std::thread detect_thread = std::thread(detect_process);
+    detect_thread.detach();
+    while(1){;}
     return 0;
 }
